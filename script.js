ef67f84cdade9b8dd5777fc039e31d4e039a2106
NaveenSharma09 <nsharma41422@gmail.com>
2025-04-16 15:22:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Dark Mode Toggle
-const toggle = document.getElementById('dark-toggle');
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('main-header');
+  const hero = document.getElementById('hero');
+  const heroHeight = hero.offsetHeight;
+
+  if (window.scrollY > heroHeight - 80) {
+    header.classList.add('light-header');
+  } else {
+    header.classList.remove('light-header');
+  }
 });
