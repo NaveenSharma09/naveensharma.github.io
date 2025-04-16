02a29f392eaa7a93ac17841e75d6979e599bbe0c
NaveenSharma09 <nsharma41422@gmail.com>
2025-04-16 15:30:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('main-header');
+  const hero = document.getElementById('hero');
+  const heroHeight = hero.offsetHeight;
+
+  if (window.scrollY > heroHeight - 80) {
+    header.classList.add('light-header');
+  } else {
+    header.classList.remove('light-header');
+  }
+});
 
